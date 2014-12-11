7b11c7475c50d7e2f44efc3391055ae4f20526a6
RJ Robinson <robertj.robinson@gmail.com>
2014-12-11 18:33:08 -0500

    added some constraints

--- number_cruncher.rb
+++ number_cruncher.rb
@@ -13,12 +13,27 @@ class Integer
   end
 end
 
+##############
+#  ROUTES    #
+##############
 
+get '/' do
+  content_type :json
+  greeting = 'Hello World. This is an api test app.'
+  instructions = "After '/'  pick a number 1 - 9999 to return a JSON response"
+  {
+    greeting: greeting,
+    instructions: instructions
+  }.to_json
+end
 
 get '/:number' do
   content_type :json
 
-  number = params[:number].to_i
+  if params[:number].to_i < 10000
+    number = params[:number].to_i
+  end
+
   { number:  number,
     factors: number.factors,
     odd:     number.odd?,
